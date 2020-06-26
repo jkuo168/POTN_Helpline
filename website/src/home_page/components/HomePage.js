5d1c4d809cf82184b34a56eeab39cd77114c2db6
jkuo168 <51718638+jkuo168@users.noreply.github.com>
2020-06-26 12:55:26 -0400

    Added menu button

--- website/src/home_page/components/HomePage.js
+++ website/src/home_page/components/HomePage.js
@@ -1,28 +1,54 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import logo from './pton_logo.png';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import logo from './pton-logo.png';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 export default function ButtonBases() {
+  const classes = useStyles();
+
   return (
     <div>
       <AppBar
         position="static"
-        style={{color: '#000000',
+        style={{
+                color: '#000000',
                 backgroundColor: '#ffffff',
                 borderStyle: 'solid',
                 borderColor: '#f58025',
                 borderWidth: '4px 0px 0px 0px'
               }}>
         <Toolbar>
-          <img src={logo}
+          <img
+                src={logo}
                 alt="Logo"
-
+                width='100'
           />
-          <Typography variant="h6">
+          <Typography
+            variant="h6"
+            style={{marginLeft: 'auto',
+                    marginRight: 'auto'
+                  }}>
             Princeton Tutoring Network Helpline
           </Typography>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
     </div>
