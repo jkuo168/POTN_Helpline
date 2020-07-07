da8a57c48beefabac222fd9d98d522427e4fc375
jkuo168 <51718638+jkuo168@users.noreply.github.com>
2020-07-06 23:55:45 -0400

    changed router

--- website/src/App.js
+++ website/src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import HomePage from "./components/home_page/HomePage";
 import HomeworkHelpline from "./components/homework_helpline/HomeworkHelpline";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route exact path="/" component={HomePage} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
