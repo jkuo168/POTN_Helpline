e593a7262f15b8df12616928ada1fab75498861c
jkuo168 <51718638+jkuo168@users.noreply.github.com>
2020-07-09 22:01:27 -0400

    finished homepage design

--- website/src/components/potn_logo/POTNLogo.js
+++ website/src/components/potn_logo/POTNLogo.js
@@ -8,6 +8,9 @@ import image3 from "./pace_3.jpg";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
+// card
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 //css for containers
 import "./index.css";
 
@@ -16,53 +19,94 @@ const styles = makeStyles((theme) => ({
     marginBottom: "5%",
     textShadow: "5px 3px 5px black",
     color: "white",
+    top: "50%",
+    left: "50%",
   },
   sign_up: {
     marginLeft: "100px",
     marginRight: "100px",
     padding: "10px 50px",
     borderRadius: "100px 100px 100px 100px",
+    borderColor: "white",
+    color: "white",
   },
   login: {
     marginLeft: "100px",
     marginRight: "100px",
     padding: "10px 57px",
     borderRadius: "100px 100px 100px 100px",
+    borderColor: "white",
+    color: "white",
   },
-  slider: {
-    infinite: "true",
-    speed: "500",
-    slidesToShow: "1",
-    slidesToScroll: "1",
+  cards: {
+    display: "flex",
+    backgroundColor: "lightgrey",
+  },
+  card: {
+    flexGrow: 1,
+    borderRadius: "0",
+    boxShadow: "0px 2px 2px 0px rgba(0,0,0,0.14)",
+    marginTop: "10px",
+    marginBottom: "10px",
+    marginLeft: "5px",
+    marginRight: "5px",
   },
 }));
 
-const renderSlides = () =>
-  [image1, image2, image3].map((image) => (
-    <div key={image}>
-      <img src={image} style={{ height: "10%" }}></img>
-    </div>
-  ));
+const cards = [
+  {
+    name: "Resources",
+  },
+  {
+    name: "Homework Helpline",
+  },
+  {
+    name: "Volunteer",
+  },
+];
 
 export default function POTNLogo() {
   const classes = styles();
-  let settings = {
-    dots: true,
-    arrows: false,
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
   };
+
   return (
     <div className="container">
-      <Slider {...settings}>{renderSlides()}</Slider>
-      <div className="centered">
-        <div className={classes.name} style={{ WebkitTextStroke: "1px grey" }}>
-          Princeton Online Tutoring Network
+      <div
+        className="text"
+        style={{
+          width: "100%",
+          height: "600px",
+          backgroundColor: "cadetblue",
+        }}
+      >
+        <div className="centered">
+          <div
+            className={classes.name}
+            style={{ WebkitTextStroke: "1px grey" }}
+          >
+            Princeton Online Tutoring Network
+          </div>
+          <ButtonBase className={classes.sign_up} variant="outlined">
+            SIGN UP
+          </ButtonBase>
+          <ButtonBase className={classes.login} variant="outlined">
+            LOGIN
+          </ButtonBase>
         </div>
-        <ButtonBase className={classes.sign_up} variant="outlined">
-          SIGN UP
-        </ButtonBase>
-        <ButtonBase className={classes.login} variant="outlined">
-          LOGIN
-        </ButtonBase>
+      </div>
+      <div className={classes.cards}>
+        {cards.map((card) => {
+          return (
+            <Card className={classes.card} key={card.name} style={{}}>
+              <CardContent>{card.name}</CardContent>
+            </Card>
+          );
+        })}
       </div>
     </div>
   );
