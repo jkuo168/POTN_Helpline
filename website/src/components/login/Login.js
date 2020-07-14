ea21b29b52b4319ca75f6d15eb47baa9ca647664
jkuo168 <51718638+jkuo168@users.noreply.github.com>
2020-07-14 16:54:03 -0400

    started login page

--- website/src/components/login/Login.js
+++ website/src/components/login/Login.js
@@ -1,7 +1,34 @@
 import React, { useCallback, useContext } from "react";
 import { withRouter, Redirect } from "react-router";
-import app from "../authentication/base.js";
 import { AuthContext } from "../authentication/Auth.js";
+import app from "../authentication/base.js";
+// styles
+import { makeStyles } from "@material-ui/core/styles";
+
+// Login card
+import Card from "@material-ui/core/Card";
+import Container from "@material-ui/core/Container";
+
+// Form
+import FormControl from "@material-ui/core/FormControl";
+import TextField from "@material-ui/core/TextField";
+
+const styles = makeStyles((theme) => ({
+  card: {
+    color: "#ffffff",
+    fontFamily: "monospace",
+    fontSize: "70px",
+    left: "50%",
+    position: "absolute",
+    top: "50%",
+    transform: "translate(-50%, -50%)",
+    border: "orange",
+    borderStyle: "solid",
+    width: "50%",
+    textAlign: "center",
+    padding: "100px",
+  },
+}));
 
 const Login = ({ history }) => {
   const handleLogin = useCallback(
@@ -26,20 +53,42 @@ const Login = ({ history }) => {
     return <Redirect to="/user_home" />;
   }
 
+  // styles
+  const classes = styles();
+
   return (
-    <div>
-      <h1>Log in</h1>
-      <form onSubmit={handleLogin}>
-        <label>
-          Email
-          <input name="email" type="email" placeholder="Email" />
-        </label>
-        <label>
-          Password
-          <input name="password" type="password" placeholder="Password" />
-        </label>
-        <button type="submit">Log in</button>
-      </form>
+    <div style={{ backgroundColor: "cadetblue", height: "100vh" }}>
+      <Card className={classes.card}>
+        <FormControl
+          style={{
+            width: "100%",
+          }}
+        >
+          <div
+            style={{
+              color: "black",
+              font: "monospace",
+              paddingBottom: "10px",
+            }}
+          >
+            Login
+          </div>
+          <div>
+            <TextField
+              label="Email"
+              variant="outlined"
+              style={{ width: "100%" }}
+            />
+          </div>
+          <div>
+            <TextField
+              label="Password"
+              variant="outlined"
+              style={{ width: "100%" }}
+            />
+          </div>
+        </FormControl>
+      </Card>
     </div>
   );
 };
